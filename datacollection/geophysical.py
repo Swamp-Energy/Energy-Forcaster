314ee87dc0ac00a39478a961b24d30b8d9c174a5
michaelkalai <michael.kalai124@gmail.com>
2025-01-25 15:07:37 -0500

    function for temperature data based on location

---
+++ datacollection/geophysical.py
@@ -0,0 +1,47 @@
+import openmeteo_requests
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+
+def get_weather_data(latitude, longitude, start_date="2022-01-01", end_date="2025-01-01"):
+    cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+    openmeteo = openmeteo_requests.Client(session = retry_session)
+
+    
+    url = "https://historical-forecast-api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "start_date": start_date,
+        "end_date": end_date,
+        "hourly": "temperature_2m"
+    }
+
+    # Fetch the weather data
+    responses = openmeteo.weather_api(url, params=params)
+    
+    # Process the first location's response
+    response = responses[0]
+    # Temporary print statements for debugging
+    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+    print(f"Elevation {response.Elevation()} m asl")
+    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+    # Process hourly data
+    hourly = response.Hourly()
+    hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+
+    hourly_data = {
+        "date": pd.date_range(
+            start = pd.to_datetime(hourly.Time(), unit="s", utc=True),
+            end = pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
+            freq = pd.Timedelta(seconds=hourly.Interval()),
+            inclusive = "left"
+        ),
+        "temperature_2m": hourly_temperature_2m
+    }
+
+    hourly_dataframe = pd.DataFrame(data=hourly_data)
+    return hourly_dataframe
